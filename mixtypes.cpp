e19377ee86d0eb6a270a80f59a8178bb57179e4d
yanfeng1012 <wyf930102@163.com>
2020-07-27 17:46:35 +0800

    [MOD] add list

---
+++ mixtypes.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+struct antarctica_years_end {
+    int year;
+};
+
+int main() {
+    antarctica_years_end s01, s02, s03;
+    s01.year = 1998;
+    antarctica_years_end *pa = &s02;
+    pa->year = 1999;
+    antarctica_years_end trios[3];
+    trios[0].year = 2003;
+    cout << trios->year << endl;
+    const antarctica_years_end *arp[3] = {&s01, &s02, &s03};
+    cout << arp[1]->year << endl;
+    const antarctica_years_end **ppa = arp;
+
+    auto ppb = arp;
+
+    cout << (*ppa)->year << endl;
+    cout << (*(ppb + 1))->year << endl;
+
+    return 0;
+}
