dbb0ad55689254bc5188eaae083eaf04e973c19d
yanfeng1012 <wyf930102@163.com>
2020-07-28 10:20:34 +0800

    [MOD] enum

---
+++ enum.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+enum {
+    red, orange, yellow, green, blue, violet, indigo
+};
+
+int main() {
+    cout << "Enter color code (0-6): ";
+    int code;
+    cin >> code;
+    while (code >= red && code <= indigo) {
+        switch (code) {
+            case red:
+                cout << "Her lips were red .\n";
+                break;
+            case orange:
+                cout << "Her har was orange .\n";
+                break;
+            case yellow:
+                cout << "Her shoes were yellow .\n";
+                break;
+            case green:
+                cout << "Her nails were green .\n";
+                break;
+            case blue:
+                cout << "Her sweatsuit were blue .\n";
+                break;
+            case violet:
+                cout << "Her eyes were violet .\n";
+                break;
+            case indigo:
+                cout << "Her mood were indigo .\n";
+                break;
+        }
+        cout << "Enter color code (0-6): ";
+        cin >> code;
+    }
+    cout << "Bye \n";
+
+    return 0;
+}
