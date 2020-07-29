f20cd93b3fca2f0d71405925548d4e248c663871
yanfeng1012 <wyf930102@163.com>
2020-07-29 16:35:04 +0800

    [MOD] struct function

---
+++ arrfun2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+const int ArSize = 8;
+
+int sum_arr(int arr[], int n);
+
+int main() {
+    int cookies[ArSize] = {1, 2, 4, 8, 16, 32, 64, 128};
+    cout << cookies << " = array address, ";
+    cout << sizeof(cookies) << " = sizeof cookies\n";
+
+    int sum = sum_arr(cookies, ArSize);
+    cout << sum << " = sum address, ";
+    cout << sizeof(sum) << " = sizeof sum\n";
+    return 0;
+}
+
+int sum_arr(int arr[], int n) {
+    int total = 0;
+    cout << arr << " = arr, ";
+    cout << sizeof(*arr) << " sizeof arr\n";
+    for (int i = 0; i < n; ++i) {
+        total += arr[i];
+    }
+    return total;
+}
---
+++ calling.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void simple();
+
+int main() {
+    cout << "main will call the simple() function:\n";
+    simple();
+
+    return 0;
+}
+
+void simple() {
+    cout << "I'm but a simple function.\n";
+}
